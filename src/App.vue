987ce31ba6a9ebe607fd85f72ee31faa8f58cef2
Alexey <alexdemichev@yandex.ru>
2022-05-17 09:54:13 +0300

    5-17 update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,90 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <input v-model="value" id="search" />
+    <button v-on:click="todo()">Добавить задачу</button>
+  </div>
+  <div v-for="todo in innerData.zadachi" v-bind:key="todo">
+    <div
+      v-if="innerData.activeFilter == 'active' && todo.active == true"
+      style="display: flex; justify-content: center"
+    >
+      <div style="margin-right: 10px">
+        {{ todo.text }}
+      </div>
+      <div v-on:click="remove(todo)">X</div>
+    </div>
+  </div>
+  <div v-for="todo in innerData.zadachi" v-bind:key="todo">
+    <div
+      v-if="innerData.activeFilter == 'all'"
+      style="display: flex; justify-content: center"
+    >
+      <div style="margin-right: 10px">
+        {{ todo.text }}
+      </div>
+      <div v-on:click="remove(todo)">X</div>
+    </div>
+  </div>
+  <div v-for="todo in innerData.zadachi" v-bind:key="todo">
+    <div
+      v-if="innerData.activeFilter == 'completed' && todo.active == false"
+      style="display: flex; justify-content: center"
+    >
+      <div style="margin-right: 10px">
+        {{ todo.text }}
+      </div>
+      <div v-on:click="remove(todo)">X</div>
+    </div>
+  </div>
+  <div>
+    <span style="margin-right: 10px" v-on:click="setFilter('active')"
+      >Активные</span
+    >
+    <span style="margin-right: 10px" v-on:click="setFilter('all')">Все</span>
+    <span style="margin-right: 10px" v-on:click="setFilter('completed')"
+      >Завершенные</span
+    >
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      innerData: {
+        zadachi: [],
+        activeFilter: "all",
+      },
+      value: "Задача 1",
+    };
+  },
+  created() {
+    function fetchData() {
+      return fetch(
+        "https://my-json-server.typicode.com/falk20/demo/todos"
+      ).then((response) => response.json());
+    }
+    fetchData().then((value) => (this.innerData.zadachi = value));
+  },
+  methods: {
+    todo() {
+      this.todo.id = Date.now();
+      this.todo.active = true;
+      this.innerData.zadachi.push({
+        text: this.value,
+        id: this.todo.id,
+        active: this.todo.active,
+      });
+    },
+    setFilter(filter) {
+      this.innerData.activeFilter = filter;
+    },
+    remove(post) {
+      this.innerData.zadachi = this.innerData.zadachi.filter(
+        (p) => p.id !== post.id
+      );
+    },
   },
 };
 </script>
